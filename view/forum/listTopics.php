882c20fdf189d85b1018d1e6a741bebc5a0720ba
Faustine67 <michelfaustine@gmail.com>
2023-03-14 10:53:49 +0100

    Forum

---
+++ view/forum/listTopics.php
@@ -0,0 +1,18 @@
+<?php
+// Exemple de comment recuperer les données envoyées par le controller //
+$topics = $result["data"]['topics'];
+    
+?>
+
+<h1>liste topics</h1>
+
+<?php
+foreach($topics as $topic ){
+
+    ?>
+    <p><?=$topic->getTitle()?></p>
+    <?php
+}
+
+
+  
