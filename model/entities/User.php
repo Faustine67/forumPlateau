b964d4eaaf4e36aa7c749252a5c071d825a734d7
Faustine67 <michelfaustine@gmail.com>
2023-03-17 16:28:22 +0100

    debeug

---
+++ model/entities/User.php
@@ -0,0 +1,110 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class User extends Entity{
+
+        private $user;
+        private $nickname;
+        private $email;
+        private $password;
+        private $inscriptionDate;
+
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->user;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($user)
+        {
+                $this->user = $user;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of nickname
+         */ 
+        public function getNickname()
+        {
+                return $this->nickname;
+        }
+
+        /**
+         * Set the value of Nickname
+         *
+         * @return  self
+         */ 
+        public function setNickname($nickname)
+        {
+                $this->topicName = $topicName;
+
+                return $this;
+        }
+
+        
+        
+        /**
+         * Get the value of EMail
+         */ 
+        public function getEmail()
+        {
+            return $this->email;
+        }
+        
+        /**
+         * Set the value of Email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+            $this->email = $email;
+            
+            return $this;
+        }
+        
+        /**
+         * Get the value of Password
+         */ 
+        public function getPassword()
+        {
+            return $this->password;
+        }
+        
+        /**
+         * Set the value of Password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+            $this->password = $password;
+            
+            return $this;
+        }
+        
+        public function getInscriptionDate(){
+                $formattedDate = $this->inscriptionDate->format("d/m/Y, H:i:s");
+                return $inscriptionDate;
+        }
+        
+        public function setInscriptiondate($inscriptionDate){
+                $this->inscriptionDate = new \DateTime($inscriptionDate);
+                return $this;
+        }
+}
